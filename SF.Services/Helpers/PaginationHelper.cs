687e73070420b0a675a96bbe6f81a669ea726240
Rostik <rostik1804@gmail.com>
2019-11-30 17:32:26 +0200

    Add GreaterThanZeroAttribute and PaginationHelper

---
+++ SF.Services/Helpers/PaginationHelper.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SF.Infrastructure;
+using SF.Services.Interfaces.CustomExceptions;
+using SF.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SF.Services.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static async Task<PagedResultDTO<TDTO>> GetPage<TEntity, TDTO>(this SFDbContext dbContext, IMapper mapper, 
+                             IQueryable<TEntity> query, int page, int pageSize) where TEntity : class where TDTO : class
+        {
+            if (page < 1)
+            {
+                throw new BadArgumentException("Page cannot be less than 1.");
+            }
+
+            if (pageSize < 1 || pageSize > 50)
+            {
+                throw new BadArgumentException("Page can contain from 1 to 50 objects.");
+            }
+
+            var countOfSkips = (page - 1) * pageSize;
+
+            var entities = await query
+                .AsNoTracking()
+                .Skip(countOfSkips)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var DTOs = mapper.Map<List<TDTO>>(entities);
+
+            var pagedResultDTO = new PagedResultDTO<TDTO>
+            {
+                Data = DTOs,
+                Page = page,
+                PageSize = pageSize,
+                TotalCount = query.Count()
+            };
+
+            return pagedResultDTO;
+        }
+    }
+}
---
+++ SF.WebAPI.Models/CustomValidations/GreaterThanZeroAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SF.WebAPI.Models.CustomValidations
+{
+    public class GreaterThanZeroAttribute : RangeAttribute
+    {
+        public GreaterThanZeroAttribute() : base(1, int.MaxValue)
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            if (value == null)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} must have value.");
+            }
+
+            if (!(value is int id))
+            {
+                return new ValidationResult($"{validationContext.DisplayName} must be integer.");
+            }
+
+            if (id >= 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult($"{validationContext.DisplayName} must be greater than zero.");
+        }
+    }
+}
